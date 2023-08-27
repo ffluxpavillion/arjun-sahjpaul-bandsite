05e3fde22f1ff9be3ac943aa462fe97d66912a1a
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-08-27 10:17:37 -0400

    feat(Comments): pulls comment data from API using axios

--- sprint-2/scripts/index.js
+++ sprint-2/scripts/index.js
@@ -3,93 +3,124 @@
 /* eslint-disable func-style */
 /* eslint-disable indent */
 
-//<-- Creates Comment Array -->
-let comments = [
-	{
-		userName: 'Connor Walton',
-		commentDate: '02/17/2021',
-		commentText:
-			'This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.',
-	},
-	{
-		userName: 'Emilie Beach',
-		commentDate: '01/09/2021',
-		commentText:
-			'I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.',
-	},
-	{
-		userName: 'Miles Acosta',
-		commentDate: '12/20/2020',
-		commentText:
-			"I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-	},
-];
+let authKey;
+const commentsArray = [];
+
+//<-- Retrieves API Authentication Key -->
+const retrieveAuthKey = () => {
+  return axios.get('https://project-1-api.herokuapp.com/register')
+    .then(response => {
+      authKey = response.data.api_key;
+    });
+};
+
+//<-- Retrieves Comments -->
+const getComments = () => {
+  return axios.get(`https://project-1-api.herokuapp.com/comments?api_key=${authKey}`)
+    .then(response => {
+      renderComments(response.data);
+    });
+};
 
 //<-- Function To Create Comment Card -->
 function createCommentCard(comment) {
-	const commentCard = document.createElement('div');
-	commentCard.classList.add('comment__card');
+  const commentCard = document.createElement('div');
+  commentCard.classList.add('comment__card');
 
-	const commentTextWrapper = document.createElement('div');
-	commentTextWrapper.classList.add('comment__text--wrapper');
+  const commentTextWrapper = document.createElement('div');
+  commentTextWrapper.classList.add('comment__text--wrapper');
 
-	const commentName = document.createElement('div');
-	commentName.classList.add('comment__name');
+  const commentName = document.createElement('div');
+  commentName.classList.add('comment__name');
 
-	const avatarDiv = document.createElement('div');
-	avatarDiv.classList.add('avatar__div');
+  const avatarDiv = document.createElement('div');
+  avatarDiv.classList.add('avatar__div');
 
-	const commentNameTxt = document.createElement('p');
-	commentNameTxt.classList.add('comment__name-txt');
-	commentNameTxt.textContent = comment.userName;
+  const commentNameTxt = document.createElement('p');
+  commentNameTxt.classList.add('comment__name-txt');
+  commentNameTxt.textContent = comment.name;
 
-	const commentDate = document.createElement('p');
-	commentDate.classList.add('comment__date');
-	commentDate.textContent = comment.commentDate;
+  const commentDate = document.createElement('p');
+  commentDate.classList.add('comment__date');
+  commentDate.textContent = formattedDate(comment.timestamp);
 
-	const commentBodyTxt = document.createElement('p');
-	commentBodyTxt.classList.add('comment__body-txt');
-	commentBodyTxt.textContent = comment.commentText;
+  const commentBodyTxt = document.createElement('p');
+  commentBodyTxt.classList.add('comment__body-txt');
+  commentBodyTxt.textContent = comment.comment;
 
-	commentName.appendChild(avatarDiv);
-	commentName.appendChild(commentNameTxt);
-	commentName.appendChild(commentDate);
+  commentName.appendChild(avatarDiv);
+  commentName.appendChild(commentNameTxt);
+  commentName.appendChild(commentDate);
 
-	commentTextWrapper.appendChild(commentName);
-	commentTextWrapper.appendChild(commentBodyTxt);
+  commentTextWrapper.appendChild(commentName);
+  commentTextWrapper.appendChild(commentBodyTxt);
 
-	commentCard.appendChild(commentTextWrapper);
-	
-	return commentCard;
+  commentCard.appendChild(commentTextWrapper);
+
+  return commentCard;
 }
 
-//<-- Function to Iterate .forEach Array Item + Insert + Append Comment Card -->
-function displayComments() {
-	const commentContainer = document.querySelector('.comment__card');
-	commentContainer.innerHTML = '';
+//<-- Formatting Date -->
+const formattedDate = timestamp => {
+  const timeNum = Number(timestamp);
+  const date = new Date(timeNum);
+  return (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
+};
 
-	comments.forEach((comment) => {
-		const commentCard = createCommentCard(comment);
-		commentContainer.appendChild(commentCard);
-	});
+//<-- Function to Display Comments -->
+function displayComments(comment) {
+  const commentContainer = document.querySelector('.parent__card');
+  const commentCard = createCommentCard(comment);
+  commentContainer.prepend(commentCard);
+	commentsArray.push(comment);
 }
 
-//<-- Function to Declare New Comment + Fetch Values From User Input -->
+//<-- New Comment Function -->
 function newComment() {
-	const userNameInput = document.getElementById('userName').value;
-	const commentTextInput = document.getElementById('commentText').value;
-	const currentDate = new Date();
+  const userNameInput = document.getElementById('userName').value;
+  const commentTextInput = document.getElementById('commentText').value;
 
-	const newComment = {
-		userName: userNameInput,
-		commentDate: currentDate.toLocaleDateString(),
-		commentText: commentTextInput,
-	};
+  const newCommentData = {
+    name: userNameInput,
+    comment: commentTextInput,
+  };
 
-	//<-- Adds New Comment At Beginning of Array -->
-	comments.unshift(newComment);
-	displayComments();
+  axios
+    .post(`https://project-1-api.herokuapp.com/comments?api_key=${authKey}`, newCommentData)
+    .then(response => {
+      //<-- Display New Comment -->
+      commentsArray.unshift(response.data);
+			displayComments(response.data);
+      
+      //<-- Clearing Field Input -->
+      document.getElementById('userName').value = '';
+      document.getElementById('commentText').value = '';
+    })
+    .catch(error => {
+      console.error("Error adding new comment:", error);
+    });
 }
 
-//<-- Call  displayComments Function -->
-displayComments();
+//<-- Renders Comments To Site -->
+const renderComments = comments => {
+  const sortedComments = comments.sort((a, b) => {
+    return b.timestamp - a.timestamp; // Fix sorting order -->
+  });
+
+  sortedComments.forEach(comment => {
+    displayComments(comment);
+  });
+};
+
+//<-- Fetching Authentication By Calling retrieveAuthKey Function  -->
+retrieveAuthKey()
+  .then(() => {
+    getComments();
+  })
+  .catch(error => {
+    console.error("Error retrieving authentication key:", error);
+	})
+  .finally(() => {
+		//<-- Log Comments Array After Fetched + Displayed -->
+    console.log("All comments:", commentsArray);
+  });
